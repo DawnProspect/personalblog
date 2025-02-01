28efbacc1502dbf3002e61f549d842c1bd733578
DawnProspect <rzaljabar@gmail.com>
2025-02-01 16:45:49 +0700

    (fix): fix default link site

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://DawnProspect.github.io",
+  site: "https://dawnprospect.github.io",
   base: "/",
   trailingSlash: "always",
   integrations: [
